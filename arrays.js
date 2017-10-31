122d5b799114fd311e93398a7be85ba712302f1f
Alexandra King <aking28@me.com>
2017-10-31 23:51:36 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -25,11 +25,8 @@ function accessElementInArray(array, index){
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array, element){
-    array.shift(element);
+    slice(1);
     return array
 }
 
-function removeElementFromBeginningOfArray(array, element){
-    array.shift(element);
-    return array
-}
+
