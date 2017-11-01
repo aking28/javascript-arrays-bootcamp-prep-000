b9ffee41c763de8b13dcf242df16d20d9f617433
Alexandra King <aking28@me.com>
2017-11-01 01:48:50 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -41,5 +41,5 @@ function destructivelyRemoveElementFromEndOfArray(array, element){
 
 
 function  removeElementFromEndOfArray(array, element){
-  return array.length
+  return array.slice(length)
 }
