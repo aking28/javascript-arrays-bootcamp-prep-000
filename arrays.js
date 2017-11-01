88546173e04038ace939ee3af922c74c6cc453c3
Alexandra King <aking28@me.com>
2017-11-01 00:17:23 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -31,6 +31,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array, element){
 
 
 function removeElementFromBeginningOfArray(array, element){
-    slice(1)
+    slice(1);
     return array
 }
